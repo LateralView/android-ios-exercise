3ecfc77b3c94946d7f4e02e53eaa579424a8a971
Leandro Tami <leandro@lateralview.net>
2016-04-07 16:51:16 -0300

    Added AddThreadVC class and storyboard design

---
+++ blinks/AddThreadVC.swift
@@ -0,0 +1,21 @@
+//
+//  AddThreadVC.swift
+//  blinks
+//
+//  Created by Leandro Tami on 4/7/16.
+//  Copyright © 2016 Lateral View. All rights reserved.
+//
+
+import UIKit
+
+class AddThreadVC: UITableViewController
+{
+    @IBOutlet weak var URL: UITextField!
+    @IBOutlet weak var details: UITextView!
+    
+    @IBAction func doSavePost(sender: AnyObject)
+    {
+        
+    }
+    
+}
